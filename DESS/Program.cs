0538bbbf12749799c2f91a8ca13eb4360f9390c3
e_ruchlewicz <ewelina.ruchlewicz@gmail.com>
2018-04-16 20:18:38 +0200

    super gui

--- DESS/Program.cs
+++ DESS/Program.cs
@@ -10,12 +10,43 @@ namespace DESS
     {
         static void Main(string[] args)
         {
-            string string64bit = "0001001100110100010101110101100110011011101111001101111111110001";
+            string string64bit = "1001001100110100010101110101100110011011101111001101111111110001";
+
+            Console.WriteLine("MENU");
+            Console.WriteLine("1 - zaszyfruj plik");
+            Console.WriteLine("2 - odszyfruj plik");
+            Console.WriteLine("3 - wyjdź\n\r");
 
             Dess dess = new Dess(string64bit);
-            dess.Encrypt("test.bin", "output.bin");
-            dess.Decrypt("output.bin", "output2.bin");
+            string input, output = "";
 
+            while (1==1)
+            {
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.WriteLine("Podaj nazwę pliku wejściowego: ");
+                        input = Console.ReadLine();
+                        Console.WriteLine("Podaj nazwę pliku wyjściowego: ");
+                        output = Console.ReadLine();
+                        dess.Encrypt(input, output);
+                        Console.WriteLine("Plik zaszyfrowano\n\r");
+                        break;
+                    case 2:
+                        Console.WriteLine("Podaj nazwę pliku wejściowego: ");
+                        input = Console.ReadLine();
+                        Console.WriteLine("Podaj nazwę pliku wyjściowego: ");
+                        output = Console.ReadLine();
+                        dess.Decrypt(input, output);
+                        Console.WriteLine("Plik odszyfrowano\n\r");
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
 
         }
     }
